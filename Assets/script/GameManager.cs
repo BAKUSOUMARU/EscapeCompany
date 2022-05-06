a7b196a0d3c473a87407a7f178618df3c5bab2d0
BAKUSOUMARU <88494642+BAKUSOUMARU@users.noreply.github.com>
2022-05-06 11:40:16 +0900

    ステージコントローラーの調整

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -6,6 +6,15 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject[] _stage;
+    [SerializeField] int _stagecount;
+    [SerializeField] GameObject _goalObject;
+    private float _stageSpawninterval;
+    private float _backScreenSpawnimterval;
+    [SerializeField] GameObject _backscreen;
+
+    int _addBackseason = 3;
+
     [SerializeField] 
     [Header("ゲームオーバー時に出てくるテキスト")]
     Text _gameOverText;
@@ -33,8 +42,43 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+    private void Start()
+    {
+        if ( _stagecount /5 >0)
+        {
+            _addBackseason += _stagecount / 4;
+        }
+        Debug.Log(_addBackseason);
+        StageSpawn();
+        BackScreenSpawn();
+    }
 
-   
+    private void Update()
+    {
+    }
+
+
+    public void StageSpawn()
+    {
+        _stageSpawninterval = 0;
+        for (int i = 0; i < _stagecount; i++)
+        {
+            int randomIndex = Random.Range(0, _stage.Length);
+            Instantiate(_stage[randomIndex], new Vector2(_stageSpawninterval, -5f), transform.rotation);
+            _stageSpawninterval += 18f;
+        }
+        Instantiate(_goalObject, new Vector2(_stageSpawninterval, -5f), transform.rotation);
+    }
+
+    public void BackScreenSpawn()
+    {
+        _backScreenSpawnimterval = (float)-14.5;
+        for (int i = 0; i < _stagecount + _addBackseason; i++)
+        {
+            Instantiate(_backscreen, new Vector3(_backScreenSpawnimterval, 0f, 10f), transform.rotation);
+            _backScreenSpawnimterval += (float)14.5;
+        }
+    }
 
     /// <summary>
     /// ゲームオーバーの処理
