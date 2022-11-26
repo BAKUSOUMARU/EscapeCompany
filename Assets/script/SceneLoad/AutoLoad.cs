9cd4595a57e26e1d99c4ae0f1268b9ebe11857a7
BAKUSOUMARU <88494642+BAKUSOUMARU@users.noreply.github.com>
2022-11-26 22:14:58 +0900

    scene遷移のコードの修正

---
+++ Assets/script/SceneLoad/AutoLoad.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+
+public class AutoLoad : SceneLoad
+{
+
+    [SerializeField]
+    [Header("ÉçÅ[ÉhÇ∑ÇÈÇ‹Ç≈ÇÃéûä‘(É~Éäïb)")]
+    int _delayTime = 5000;
+
+    private CancellationTokenSource _cts = new CancellationTokenSource();
+    void Start()
+    {
+        StartLoad(_cts.Token);
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            _cts.Cancel();
+        }    
+    }
+    async UniTask StartLoad(CancellationToken ct = default)
+    {
+        try
+        {
+            await UniTask.Delay(_delayTime,cancellationToken: ct);
+        }
+        catch(System.OperationCanceledException)
+        {
+            throw;
+        }
+        finally
+        {
+            LoadScene();
+        }
+    }
+}
--- Assets/script/SceneLoad/StageSelectLoad.cs
+++ Assets/script/SceneLoad/StageSelectLoad.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StageSelectLoad : MonoBehaviour
+public class StageSelectLoad : SceneLoad
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    [Header("遷移される先の難易度の設定")]
+    StageLevel _stagelevel;
+
+    [SerializeField] GameObject _popup;
+
+    [SerializeField]
+    [Header("フリーレベルの入力されるText")]
+    Text _inpotText;
+    public void SelectStage()
     {
-        
+        GameManager.instance.GameReset();
+        switch (_stagelevel)
+        {
+            case StageLevel.Normal:
+                GameManager.instance.stagecount = 20;
+                GameManager.instance.isfreelevel = false;
+                GameManager.instance.freelevel = 0;
+                LoadScene();
+                break;
+
+            case StageLevel.hard:
+                GameManager.instance.stagecount = 40;
+                GameManager.instance.isfreelevel = false;
+                GameManager.instance.freelevel = 0;
+                LoadScene();
+                break;
+
+            case StageLevel.freelevel:
+                if (int.Parse(_inpotText.text) > 0 || _inpotText.text == null)
+                {
+                    LoadScene();
+                    GameManager.instance.freelevel = int.Parse(_inpotText.text);
+                    GameManager.instance.isfreelevel = true;
+                }
+                else
+                {
+                    _popup.SetActive(true);
+                }
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    enum StageLevel
     {
-        
+        Normal,
+        hard,
+        freelevel
     }
 }
