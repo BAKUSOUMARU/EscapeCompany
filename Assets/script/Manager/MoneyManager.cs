6132378e4900fa52d9501538009fdd8ea58c081b
BAKUSOUMARU <88494642+BAKUSOUMARU@users.noreply.github.com>
2022-12-06 03:03:08 +0900

    Jsonの実装途中

--- Assets/script/Manager/MoneyManager.cs
+++ Assets/script/Manager/MoneyManager.cs
@@ -1,6 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class SaveData
+{
+    public int Money;
+}
 
 public class MoneyManager:SingletonMonoBehaviour<MoneyManager>
 {
@@ -9,14 +16,29 @@ public class MoneyManager:SingletonMonoBehaviour<MoneyManager>
     [SerializeField]
     private int _money = 0;
 
+    [SerializeField]
+    private string _filePath;
+
+    [SerializeField]
+    SaveData _saveData;
+
+    private void Awake()
+    {
+        _filePath = Application.persistentDataPath + "/" + ".savedata.json";
+    }
 
     public void MoneyUp(int addMoney)
     {
         _money += addMoney;
     }
 
-     public void MoneyDown(int downMoney)
+    public void MoneyDown(int downMoney)
     {
         _money -= downMoney+1;
+    }
+
+    public void Save()
+    {
+        string Json = JsonUtility.ToJson(_saveData);
     }
 }
